452be0d436f86752001bb00cd537b96d77e634f3
HsiaoCz <984274788@qq.com>
2023-05-07 14:15:19 +0800

    some skills

---
+++ 02skills/01decorator/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type DB interface {
+	Store(string) error
+}
+
+type Store struct{}
+
+func (s *Store) Store(value string) error {
+	fmt.Println("storing into DB", value)
+	return nil
+}
+
+func myExecuteFunc(db DB) ExecuteFn {
+	return func(s string) {
+		fmt.Println("my ex func", s)
+		db.Store(s)
+	}
+}
+
+func makeHTTPFunc(db DB, fn httpFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := fn(db, w, r); err != nil {
+			//
+		}
+	}
+}
+
+func main() {
+	s := &Store{}
+	http.HandleFunc("/", makeHTTPFunc(s, handler))
+	Execute(myExecuteFunc(s))
+}
+
+func handler(db DB, w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+type httpFunc func(db DB, w http.ResponseWriter, r *http.Request) error
+
+type ExecuteFn func(string)
+
+func Execute(fn ExecuteFn) {
+	fn("FOO BAR BAZ")
+}
