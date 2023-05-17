f5c951a064188acf571c7f871c06e97d5b518d69
HsiaoCz <984274788@qq.com>
2023-05-18 01:47:33 +0800

    template

---
+++ 05gin/01template/template.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type User struct {
+	Name   string
+	Age    int
+	Gender string
+	Info   map[string]string
+}
+
+func main() {
+	http.HandleFunc("/user", sayHello)
+	http.ListenAndServe(":9090", nil)
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("./hello.html")
+	if err != nil {
+		fmt.Println("create template failed, err:", err)
+		return
+	}
+	user := User{
+		Name:   "Alex",
+		Gender: "nan",
+		Age:    11,
+		Info: map[string]string{
+			"hello":   "hi",
+			"bob":     "alic",
+			"message": "bil",
+		},
+	}
+	tmpl.Execute(w, user)
+}
