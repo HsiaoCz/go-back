0bf9ad3a4ccf130b483bdf4028d22738486ae5ed
HsiaoCz <984274788@qq.com>
2023-05-26 14:22:15 +0800

    pattern

---
+++ 02skills/08strategy/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// 策略模式
+// 这里举个例子
+// 英雄使用武器
+type WeaponStategy interface {
+	UseWeapon()
+}
+
+// 具体的策略
+type Ak47 struct{}
+
+func (ak *Ak47) UseWeapon() { fmt.Println("使用Ak47去战斗...") }
+
+type Knife struct{}
+
+func (k *Knife) UseWeapon() { fmt.Println("使用小刀去战斗...") }
+
+// 环境类
+type Hero struct {
+	strategy WeaponStategy //环境类拥有抽象的策略
+}
+
+// 设置一个策略
+func (h *Hero) SetWeaponStategy(s WeaponStategy) {
+	h.strategy = s
+}
+
+func (h *Hero) Fight() {
+	h.strategy.UseWeapon() // 在这里使用策略
+}
