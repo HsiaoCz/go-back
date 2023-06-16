465387f52b3931181ab1edcfa9b6c499c28b515f
HsiaoCz <984274788@qq.com>
2023-06-16 09:05:59 +0800

    add params

---
+++ 07use-pattern/14adapter/main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+// 适配器模式
+// 将一个类的接口转换成客户希望的另外一个接口
+// 使得原本因为接口不兼容而不能一起工作的那些类可以一起工作
+
+// 这里举个iphone充电器的例子
+
+type V5 interface {
+	Use5V()
+}
+
+type Phone struct {
+	v V5
+}
+
+func NewPhone(v V5) *Phone {
+	return &Phone{
+		v: v,
+	}
+}
+
+func (p *Phone) Charge() {
+	fmt.Println("对Phone进行充电")
+	p.v.Use5V()
+}
+
+// 被适配的角色
+type V220 struct{}
+
+func (v *V220) Use220V() {
+	fmt.Println("使用220v的电压进行充电")
+}
+
+// 电源适配器
+type Adapter struct {
+	v220 *V220
+}
+
+func (a *Adapter) Use5V() {
+	fmt.Println("使用电源适配器进行充电")
+	a.v220.Use220V()
+}
+
+func NewAdapter(v *V220) *Adapter {
+	return &Adapter{
+		v220: v,
+	}
+}
+
+func main() {
+	iphone := NewPhone(NewAdapter(new(V220)))
+	iphone.Charge()
+}
