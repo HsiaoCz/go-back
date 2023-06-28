551ece2fb41d27fc2cb2b148c0f9a4c612d7392b
HsiaoCz <984274788@qq.com>
2023-06-28 08:17:07 +0800

    kit

--- 13kit-srv/endpoint/endpoint.go
+++ 13kit-srv/endpoint/endpoint.go
@@ -1,4 +1,52 @@
 package endpoint
 
+import (
+	"context"
+	"go-back/13kit-srv/service"
+
+	"github.com/go-kit/kit/endpoint"
+)
+
 // endpoint负责存放项目对外暴露的RPC方法
 
+type SumRequest struct {
+	A int `json:"a"`
+	B int `json:"b"`
+}
+
+type SumResponse struct {
+	V   int    `json:"v"`
+	Err string `json:"err,omitempty"`
+}
+
+type ConcatRequest struct {
+	A string `json:"a"`
+	B string `json:"b"`
+}
+
+type ConcatResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"`
+}
+
+func MakeSumEndpoint(srv service.AddService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(SumRequest)
+		v, err := srv.Sum(ctx, req.A, req.B)
+		if err != nil {
+			return SumResponse{V: v, Err: err.Error()}, nil
+		}
+		return SumResponse{V: v}, nil
+	}
+}
+
+func MakeConcatEnpoint(srv service.AddService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(ConcatRequest)
+		v, err := srv.Concat(ctx, req.A, req.B)
+		if err != nil {
+			return ConcatResponse{V: v, Err: err.Error()}, nil
+		}
+		return ConcatResponse{V: v}, nil
+	}
+}
