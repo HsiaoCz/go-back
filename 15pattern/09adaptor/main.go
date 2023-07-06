5cbb5b60174c67133868fe70228e81d9e449afae
HsiaoCz <984274788@qq.com>
2023-07-06 20:17:15 +0800

    add pattern

---
+++ 15pattern/09adaptor/main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+// 适配器模式
+// 将一个类的接口转换成客户希望的另一个接口
+
+type V5 interface {
+	Use5V()
+}
+
+// 业务类
+// 依赖5V接口
+type Phone struct {
+	v V5
+}
+
+func NewPhone(v V5) *Phone {
+	return &Phone{v: v}
+}
+
+func (p *Phone) Charge() {
+	fmt.Println("对Phone进行充电...")
+	p.v.Use5V()
+}
+
+// 被适配的角色
+type V220 struct{}
+
+func (v *V220) Use220V() {
+	fmt.Println("使用220v的电压进行充电...")
+}
+
+// 电源适配器
+type Adapter struct {
+	v220 *V220
+}
+
+func (a *Adapter) Use5V() {
+	fmt.Println("使用电源适配器进行充电")
+	a.v220.Use220V()
+}
+
+func NewAdapter(v220 *V220) *Adapter {
+	return &Adapter{v220: v220}
+}
+
+func main() {
+	iphone := NewPhone(NewAdapter(new(V220)))
+	iphone.Charge()
+}
