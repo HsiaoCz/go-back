f99dca4eb352c5c5052ed84871fa8d22b4618486
HsiaoCz <984274788@qq.com>
2023-07-13 20:04:18 +0800

    proxy

--- 15pattern/17decorator/main.go
+++ 15pattern/17decorator/main.go
@@ -4,23 +4,16 @@ import "fmt"
 
 // 装饰器模式
 // 动态的给一个对象增加一些额外的职责
-
-// 这里拿手机举个例子，给一台手机贴膜和戴壳
-
-// 抽象的手机
 type Phone interface {
 	Show()
 }
 
 // 装饰器的基础类
-// 这里为结构体是因为go语言中interface不可以有成员属性
 type Decorator struct {
 	phone Phone
 }
 
 func (d *Decorator) Show() {}
-
-// 具体的手机
 
 type XiaoMi struct{}
 
@@ -30,7 +23,7 @@ type HuaWei struct{}
 
 func (h *HuaWei) Show() { fmt.Println("秀出了华为手机...") }
 
-// 具体的装饰器类
+// 具体的装饰类
 type MoDecorator struct {
 	Decorator // 继承基础的装饰器类(主要继承phone的成员属性)
 }
@@ -41,7 +34,7 @@ func NewMoDecorator(phone Phone) Phone {
 
 func (md *MoDecorator) Show() {
 	md.phone.Show()
-	fmt.Println("戴上了手机壳的手机...")
+	fmt.Println("戴上了壳的手机")
 }
 
 type Kedecorator struct {
