11799891d52b34ccd559e56d7dac6502221bc2d8
HsiaoCz <984274788@qq.com>
2023-07-23 08:17:15 +0800

    grpc interceptor

--- grpc/interc/client/main.go
+++ grpc/interc/client/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"go-back/grpc/interc/client/pb"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+// 基于拦截器和metadata 实现token
+func main() {
+	conn, err := grpc.Dial("127.0.0.1:9091", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	c := pb.NewHelloClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.Login(ctx, &pb.LoginRequest{Username: "zhangsan", Password: "admin"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("reply:", r)
+}
--- grpc/interc/server/main.go
+++ grpc/interc/server/main.go
@@ -1,2 +1,36 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"go-back/grpc/interc/client/pb"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+// server
+// 基于拦截器和metadata 实现token
+type server struct {
+	pb.UnimplementedHelloServer
+}
+
+func (s *server) Login(ctx context.Context, in *pb.LoginRequest) (*pb.LoginResponse, error) {
+	return nil, nil
+}
+
+func main() {
+	lisn, err := net.Listen("tcp", ":9091")
+	if err != nil {
+		fmt.Printf("failed to listen:%v", err)
+		return
+	}
+	s := grpc.NewServer()
+	pb.RegisterHelloServer(s, &server{})
+	// 启动服务
+	err = s.Serve(lisn)
+	if err != nil {
+		fmt.Printf("failed to serve:%v", err)
+		return
+	}
+}
