418b16e5a3af472d56180b68de68521ac3e1d71d
HsiaoCz <984274788@qq.com>
2023-07-24 13:06:22 +0800

    grpc interceptor

--- grpc/interc/client/main.go
+++ grpc/interc/client/main.go
@@ -9,10 +9,25 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 )
+
 // 基于拦截器和metadata 实现token
+
+func InterToken(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	md := metadata.New(map[string]string{
+		"addid":  "101010",
+		"appkey": "this is key",
+	})
+	ctx = metadata.NewOutgoingContext(context.Background(), md)
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	return err
+}
+
 func main() {
-	conn, err := grpc.Dial("127.0.0.1:9091", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// 注册一个拦截器
+	opt := grpc.WithUnaryInterceptor(InterToken)
+	conn, err := grpc.Dial("127.0.0.1:9091", grpc.WithTransportCredentials(insecure.NewCredentials()), opt)
 	if err != nil {
 		log.Fatal(err)
 	}
--- grpc/interc/server/main.go
+++ grpc/interc/server/main.go
@@ -7,6 +7,9 @@ import (
 	"net"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
 // server
@@ -19,13 +22,40 @@ func (s *server) Login(ctx context.Context, in *pb.LoginRequest) (*pb.LoginRespo
 	return nil, nil
 }
 
+func InterToken(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return resp, status.Error(codes.Unauthenticated, "无token认真")
+	}
+	var (
+		appid  string
+		appkey string
+	)
+
+	if va1, ok := md["appid"]; ok {
+		appid = va1[0]
+	}
+	if va2, ok := md["appkey"]; ok {
+		appkey = va2[0]
+	}
+	if appid != "101010" || appkey != "this is key" {
+		return resp, status.Error(codes.Unauthenticated, "认证错误")
+	}
+	res, err := handler(ctx, req)
+	return res, err
+}
+
 func main() {
 	lisn, err := net.Listen("tcp", ":9091")
 	if err != nil {
 		fmt.Printf("failed to listen:%v", err)
 		return
 	}
-	s := grpc.NewServer()
+	// 注册一个拦截器
+	opt := grpc.UnaryInterceptor(InterToken)
+	// 创建grpc服务
+	s := grpc.NewServer(opt)
+	// 在grpc服务端注册服务
 	pb.RegisterHelloServer(s, &server{})
 	// 启动服务
 	err = s.Serve(lisn)
