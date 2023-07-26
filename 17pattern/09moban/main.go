0833729feb4e05e8046263f393a77e34b8fc58e9
HsiaoCz <984274788@qq.com>
2023-07-26 21:55:03 +0800

    anyone else

---
+++ 17pattern/09moban/main.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+// 命令模式
+// 将一个请求封装成一个对象，从而让我们可以不用的请求对客户进行参数化
+// 命令模式可以将请求发送者和接收者完全解耦，发送者和接收者之间没有直接引用
+// 发送请求的对象只需要知道如何发送请求，而不必知道如何完成请求
+
+type Cooker struct{}
+
+func (c *Cooker) MakeChuaner() { fmt.Println("烤串师傅烤了羊肉串") }
+func (c *Cooker) MakeJiChi()   { fmt.Println("烤串师傅烤了鸡翅") }
+
+// 抽象的命令
+type Command interface{ Make() }
+
+type CommandCookChicken struct {
+	cooker *Cooker
+}
+
+func (c *CommandCookChicken) Make() {
+	c.cooker.MakeJiChi()
+}
+
+type CommandCookChuaner struct {
+	cooker *Cooker
+}
+
+func (ccc *CommandCookChuaner) Make() {
+	ccc.cooker.MakeChuaner()
+}
+
+// 服务员，命令的收集者
+type Waiter struct {
+	CmdList []Command
+}
+
+func (w Waiter) Notify() {
+	if w.CmdList == nil {
+		return
+	}
+	for _, cmd := range w.CmdList {
+		cmd.Make()
+	}
+}
+func main() {
+	cooker := new(Cooker)
+	cmdChicken := CommandCookChicken{cooker: cooker}
+	cmdChuaner := CommandCookChuaner{cooker: cooker}
+
+	waiter := new(Waiter)
+	waiter.CmdList = append(waiter.CmdList, &cmdChicken)
+	waiter.CmdList = append(waiter.CmdList, &cmdChuaner)
+
+	waiter.Notify()
+}
