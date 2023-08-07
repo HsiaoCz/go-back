8a8bbe498d9d39b07966e968c17edc99f5bdeb17
HsiaoCz <984274788@qq.com>
2023-08-07 21:21:05 +0800

    backagen

---
+++ 20pattern/adaptor.go
@@ -0,0 +1,46 @@
+package main
+
+ import "fmt"
+
+// 将一个类的接口转换成客户希望的另外一个接口
+// 使得原本因为接口不兼容而不能一起工作的那些类可以一起工作
+
+type V5 interface {
+	Use5V()
+}
+
+// 业务类，依赖5v接口
+type IPhone struct {
+	v V5
+}
+
+func NewPhone(v V5) *IPhone {
+	return &IPhone{v: v}
+}
+
+func (i *IPhone) Charge() {
+	fmt.Println("对iphone进行充电")
+	i.v.Use5V()
+}
+
+// 被适配的角色
+type V220 struct{}
+
+func (v *V220) Use220V() {
+	fmt.Println("使用220v的电压进行充电")
+}
+
+// 电源适配器
+type Adapter struct {
+	v220 *V220
+}
+
+func (a *Adapter) Use5V() {
+	fmt.Println("使用电源适配器进行充电..")
+	a.v220.Use220V()
+}
+
+func NewAdapter(v220 *V220) *Adapter {
+	return &Adapter{v220: v220}
+}
+
---
+++ 20pattern/decorator.go
@@ -0,0 +1,58 @@
+package main
+
+// 装饰器模式
+
+import "fmt"
+
+// 装饰器模式
+// 动态的为一个对象增加一些额外的职责
+
+type Phone interface {
+	Show()
+}
+
+// 装饰器的基础类
+type Decorator struct {
+	phone Phone
+}
+
+func (d *Decorator) Show() {}
+
+// 具体的手机
+
+type XiaoMi struct{}
+
+func (x *XiaoMi) Show() { fmt.Println("秀出了小米手机") }
+
+type HuaWei struct{}
+
+func (h *HuaWei) Show() { fmt.Println("秀出了华为手机") }
+
+// 具体的装饰器
+type MoDecorator struct {
+	Decorator
+}
+
+func NewMoDecorator(phone Phone) Phone {
+	return &MoDecorator{Decorator{phone: phone}}
+}
+
+func (md *MoDecorator) Show() {
+	md.phone.Show()
+	fmt.Println("戴上了壳的手机")
+}
+
+// 具体的装饰器
+type keDecorator struct {
+	Decorator
+}
+
+func NewKeDecorator(phone Phone) Phone {
+	return &keDecorator{Decorator{phone: phone}}
+}
+
+func (kd *keDecorator) Show() {
+	kd.phone.Show()
+	fmt.Println("戴上壳的手机")
+}
+
