e5a91c2c2dd0488242fc0af91f883a9b61d47205
HsiaoCz <984274788@qq.com>
2023-08-10 22:19:55 +0800

    backagen

---
+++ 22pattern/simplefact.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+// 简单工厂模式
+// 抽象的水果类
+type Fruit interface {
+	Show()
+}
+
+// 具体的水果类
+type Apple struct{}
+
+func (a *Apple) Show() { fmt.Println("this is apple") }
+
+type Banana struct{}
+
+func (b *Banana) Show() { fmt.Println("this is banana") }
+
+type Pear struct{}
+
+func (p *Pear) Show() { fmt.Println("this is pear") }
+
+// 工厂类
+type SimpleFactory struct{}
+
+func (s *SimpleFactory) CreateFruit(kind string) Fruit {
+	var fruit Fruit
+	if kind == "apple" {
+		fruit = new(Apple)
+	}
+	if kind == "banana" {
+		fruit = new(Banana)
+	}
+	if kind == "pear" {
+		fruit = new(Pear)
+	}
+	return fruit
+}
