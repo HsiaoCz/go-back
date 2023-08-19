dc8473c89c75b894ab13f57ca62b3a36021f7bdc
HsiaoCz <984274788@qq.com>
2023-08-19 19:50:14 +0800

    backagen

---
+++ 21standerd/02file/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+// go 文件操作
+
+func main() {
+	// 打开文件
+	file, err := os.Open("./1233.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+	b := make([]byte, 1024)
+	n, err := file.Read(b)
+	if err == io.EOF {
+		fmt.Println("文件读取完毕")
+		return
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("读取的文件的内容")
+	fmt.Println(string(b[:n]))
+}
+
+// 通过bufio读取文件的内容
+func ReadFileByBuffIO(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err == io.EOF {
+			if len(line) != 0 {
+				fmt.Println(line)
+				return
+			}
+		}
+		fmt.Println(line)
+	}
+}
+
+// ReadFile 方法可以读取全部的文件
+func ReadFileAll(filename string) {
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(b))
+}
+
+// 文件操作
+func WriteFile(filename string) {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	str := "yizhi"
+	file.Write([]byte(str))       // 写入字节
+	file.WriteString("mammamamm") // 写入字符串
+}
+
+// 使用bufio的NewWriter
+func WriteFileByBuffio(filename string) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	write := bufio.NewWriter(file)
+	for i := 0; i < 10; i++ {
+		write.WriteString("hahhahh ,mamm\n")
+	}
+	write.Flush()
+}
+
+// os.WriteFile
+func WriteFileByOs(filename string) {
+	err := os.WriteFile(filename, []byte("meizi"), 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
