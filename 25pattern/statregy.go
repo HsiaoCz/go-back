15ff08159b32793c470d70b43b54724d66097e0c
HsiaoCz <984274788@qq.com>
2023-08-20 22:57:25 +0800

    backagen

---
+++ 25pattern/statregy.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// 策略模式
+type WeaponStategy interface {
+	UseWeapon() // 使用武器
+}
+
+// 具体的策略
+type Ak47 struct{}
+
+func (ak *Ak47) UseWeapon() { fmt.Println("使用AK47去战斗") }
+
+type Knife struct{}
+
+func (k *Knife) UseWeapon() { fmt.Println("使用小刀去战斗...") }
+
+// 环境类
+type Hero struct {
+	strategy WeaponStategy // 环境类拥有抽象的策略
+}
+
+// 设置一个策略
+func (h *Hero) SetWeaponStategy(s WeaponStategy) {
+	h.strategy = s
+}
+
+func (h *Hero) Fight() {
+	h.strategy.UseWeapon() // 在这里使用策略
+}
